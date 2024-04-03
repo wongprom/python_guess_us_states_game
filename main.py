adc9b2dcda8bf71b50e89bda0cf71a2d8eb43127
wongprom <>
2024-04-03 23:06:51 +0200

    Create input, save value, print value.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("US States Game")
@@ -7,4 +8,13 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
+answer_state = screen.textinput(title="Guess the State", prompt="Whats another state name?")
+print(answer_state)
+
+# Get coor when click on image
+# def get_mouse_click_coor(x, y):
+#     print(x, y)
+# turtle.onscreenclick(get_mouse_click_coor)
+# turtle.mainloop()
+#
 screen.exitonclick()
