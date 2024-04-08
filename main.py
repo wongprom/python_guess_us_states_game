0efc394dc0eabec2aea7c2ef4743c867b7cc0e88
wongprom <>
2024-04-08 14:08:36 +0200

    Add game functionality...
    1.Check if guess is among 50 states. 2 Write correct guess on map. 3.Add correct gusse in array. Keep track on the score.

--- main.py
+++ main.py
@@ -7,9 +7,26 @@ screen.title("US States Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
+state_text = turtle.Turtle()
+state_text.pencolor("red")
 
-answer_state = screen.textinput(title="Guess the State", prompt="Whats another state name?")
-print(answer_state)
+states_data = pandas.read_csv("50_states.csv")
+guessed_stats = []
+
+
+while len(guessed_stats) < 50:
+    answer_state = screen.textinput(title=f"Guess the State {len(guessed_stats)}/{len(states_data)}", prompt="Whats another state name?").title()
+
+    for state in states_data.state:
+        if state == answer_state:
+            correct_state_row_data = states_data[states_data.state == answer_state]
+            x_corr = int(correct_state_row_data.x.iloc[0])
+            y_corr = int(correct_state_row_data.y.iloc[0])
+
+            state_text.goto(x_corr, y_corr)
+            state_text.write(answer_state)
+            guessed_stats.append(answer_state)
+
 
 # Get coor when click on image
 # def get_mouse_click_coor(x, y):
